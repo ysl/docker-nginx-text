eff24cddf365141a9c60a8f22450f230d0ef8e30
Brian Lin <ys.ncku@gmail.com>
2022-11-15 13:37:42 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
 
-docker build -t brianatisw/nginx-test .
+# Should specify platform to run on AWS ECS.
+docker buildx build --platform=linux/amd64 -t brianatisw/nginx-test .
 docker images|grep brianatisw/nginx-test
